7dfe76859d46ac326abbfcc104afc91aa4712f55
Cody Hannah <chinnc@mail.gvsu.edu>
2025-04-16 14:59:34 -0400

    Adding hurt sprite animation to player

--- GodotTesting/im-learning/Scripts/player.gd
+++ GodotTesting/im-learning/Scripts/player.gd
@@ -6,9 +6,6 @@ const JUMP_VELOCITY = -300.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	#Check for damage
-	if animated_sprite.animation == "hurt" and animated_sprite.is_playing():
-		return
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -29,14 +26,16 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0:
 		animated_sprite.flip_h = true
 	
-	if is_on_floor():
+	if animated_sprite and animated_sprite.animation=="hit" and animated_sprite.is_playing():
+		pass
+	elif is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run") 
 	else: 
 		animated_sprite.play("jump")
-		
+			
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -45,4 +44,4 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func hit():
-	animated_sprite.play("hurt")
+	animated_sprite.play("hit")
